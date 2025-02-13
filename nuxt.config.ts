4e7c2f648a5c3dd6a0241d24574fda709133a81d
Lukas Trumm <lukas.trumm@gmail.com>
2025-02-13 19:33:31 +0100

    Nuxt project initialized with complete setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,47 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: [
+    "@lttr/nuxt-config-postcss",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/plausible",
+    "@nuxtjs/seo",
+    "@vueuse/nuxt",
+  ],
   devtools: { enabled: true },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  css: ["@lttr/puleo", "~/assets/css/main.css"],
+  eslint: {
+    config: {
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+  site: {
+    url: "https://example.com",
+    name: "Website name",
+    description: "Website description",
+    defaultLocale: "en",
+  },
   future: {
-    compatibilityVersion: 4
-  }
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    typedPages: true,
+  },
+  compatibilityDate: "2025-02-01",
+  lttrConfigPostcss: {
+    filesWithGlobals: ["./node_modules/@lttr/puleo/output/media.css"],
+  },
+  plausible: {
+    ignoredHostnames: ["localhost"],
+    apiHost: "https://plausible.lttr.cz",
+  },
 })
